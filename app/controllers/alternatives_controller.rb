3e2762dc4b31c181e9bbdebb981da8f06698f61d
Romain Hethener <romain.hethener@gmail.com>
2019-12-03 16:58:06 +0100

    new/create working

--- app/controllers/alternatives_controller.rb
+++ app/controllers/alternatives_controller.rb
@@ -1,9 +1,42 @@
 class AlternativesController < ApplicationController
+  before_action :set_params, only: %i[show, edit]
+
   def index
     @alternatives = Alternative.all
   end
 
   def show
+  end
+
+  def new
+    @alternative = Alternative.new
+  end
+
+  def create
+    @alternative = Alternative.new(alternative_params)
+    @alternative.user = current_user
+    if @alternative.save
+      redirect_to alternative_path(@alternative)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
     @alternative = Alternative.find(params[:id])
+    @alternative.update(alternative_params)
+  end
+
+  private
+
+  def set_alternatives
+    @alternative = Alternative.find(params[:id])
+  end
+
+  def alternative_params
+    params.require(:alternative).permit(:title, :description, :ingredients, :difficulty, :prep_time)
   end
 end
