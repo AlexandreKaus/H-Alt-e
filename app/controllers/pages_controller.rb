2284ad7d56b5f9b5eacca1e6ce8637947b3185eb
Alexandre Kaus <kaus.alexandre@gmail.com>
2019-12-04 18:18:27 +0100

    on met à jour

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,27 +7,27 @@ class PagesController < ApplicationController
 
   def dashboard
     @user = current_user
-    raise
 
-    alternatives = Alternative.all
-    @alternatives_select = alternatives.select do |alternative|
-      alternative.user == @user
-    end
 
-    reviews = Review.all
-    @reviews_select = reviews.select do |review|
-      review.user == @user
-    end
+  #   alternatives = Alternative.all
+  #   @alternatives_select = alternatives.select do |alternative|
+  #     alternative.user == @user
+  #   end
 
-    upvoted_posts = Upvote.all
-    @alternatives_upvoted = []
+  #   reviews = Review.all
+  #   @reviews_select = reviews.select do |review|
+  #     review.user == @user
+  #   end
 
-    @upvoted_posts_select = upvoted_posts.select do |upvoted_post|
-      upvoted_post.user == @user
-    end
+  #   upvoted_posts = Upvote.all
+  #   @alternatives_upvoted = []
 
-    @upvoted_posts_select.each do |up|
-      @alternatives_upvoted << up.alternative
-    end
+  #   @upvoted_posts_select = upvoted_posts.select do |upvoted_post|
+  #     upvoted_post.user == @user
+  #   end
+
+  #   @upvoted_posts_select.each do |up|
+  #     @alternatives_upvoted << up.alternative
+  #   end
   end
 end
